2ef88133b499172ae9d88862d2a1da25362f7e0b
MichelleCdn <michellecdn.tab2@gmail.com>
2023-12-29 05:43:53 +0700

    tambahan layout

--- lib/pages/main/JadwalPage.dart
+++ lib/pages/main/JadwalPage.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:dolan/class/detailjadwal.dart';
+import 'package:dolan/pages/main/BuatJadwalPage.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class JadwalPage extends StatefulWidget {
   const JadwalPage({super.key});
@@ -8,8 +13,137 @@ class JadwalPage extends StatefulWidget {
 }
 
 class _JadwalPageState extends State<JadwalPage> {
+  // DetailJadwal? _dj;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  void initState() {
+    super.initState();
+    bacaData();
   }
+
+
+  Future<String> fetchData() async {
+    final response = await http
+      .post(Uri.parse("https://hybrid.anxdre.my.id/api/auth/jadwal")
+    );
+    if (response.statusCode == 200) {
+    return response.body;
+    } else {
+    throw Exception('Failed to read API');
+    }
+  }
+
+ bacaData() {
+    DJs.clear();
+    Future<String> data = fetchData();
+    data.then((value) {
+      Map json = jsonDecode(value);
+      for (var jadwal in json['data']) {
+        DetailJadwal dj = DetailJadwal.fromJSON(jadwal);
+        DJs.add(dj);
+      }
+      setState(() {
+        // _temp = PMs[2].overview;
+        for (var jadwal in json['data']) {
+          DetailJadwal dj = DetailJadwal.fromJSON(jadwal);
+          DJs.add(dj);
+        }
+      });
+    });
+  }
+
+  
+
+Widget DaftarJadwal(DaftarJadwal) {
+    if (DaftarJadwal != null) {
+      return ListView.builder(
+          itemCount: DaftarJadwal.length,
+          itemBuilder: (BuildContext ctxt, int index) {
+            int jumlahPemain = DJs[index].pemain.length;
+            return Card(
+                child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  leading: Image.network(
+                      DaftarJadwal[index].foto,
+                      width: 50, // Sesuaikan lebar gambar sesuai kebutuhan
+                      height: 50, // Sesuaikan tinggi gambar sesuai kebutuhan
+                      fit: BoxFit.cover, // Sesuaikan jenis penyesuaian gambar sesuai kebutuhan
+                    ),
+                  
+                  title: GestureDetector(
+                      child: Text(DJs[index].informasiDolanan)),
+
+                  // subtitle: Text(PopMovs[index].overview),
+
+                   subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(DJs[index].tanggal),
+                        Text(DJs[index].jam),
+                        SizedBox(height: 4),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Action when the button is pressed
+
+
+                          },
+                          //ubah text button
+                          child: Text('$jumlahPemain / ${DJs[index].jumlahPemain} orang'),
+
+                        ),
+
+
+                        Text(DJs[index].namaTempat),
+                        Text(DJs[index].alamat),
+                       
+                    
+                        SizedBox(height: 5),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Action when the button is pressed
+
+
+                          },
+                          child: Text('Party Chat'),
+                        ),
+                      ],
+                    ),
+                ),
+              ],
+            ));
+          });
+    } else {
+      return Center(
+        child: Text('Jadwal main masih kosong nih. Cari konco main atau bikin jadwal baru aja'),
+      );
+    }
+  }
+
+
+ @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: ListView(
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.height - 200,
+          child: DaftarJadwal(DJs),
+        ),
+      ],
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => BuatJadwalPage()),
+        );
+   
+      },
+      child: Icon(Icons.create_outlined),
+    ),
+  );
+}
+
 }
--- lib/pages/main/ProfilPage.dart
+++ lib/pages/main/ProfilPage.dart
@@ -8,8 +8,86 @@ class ProfilPage extends StatefulWidget {
 }
 
 class _ProfilPageState extends State<ProfilPage> {
+
+//ini diganti menjadi isian sebelumnya (tidak kosong)
+  String _photoUrl = "";
+  String _nama="";
+
+  //email nanti diganti menjadi tidak dapat diedit
+  String _email="";
+
+ // Controller untuk mengelola input nama
+  TextEditingController _nameController = TextEditingController();
+
+ @override
+  void initState() {
+    super.initState();
+    _nameController.text = _nama;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Menampilkan gambar profil
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(_photoUrl),
+            ),
+            SizedBox(height: 10),
+
+            // Input untuk mengubah nama
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Nama Lengkap',
+              ),
+            ),
+            SizedBox(height: 5),
+
+            TextFormField(
+              initialValue: _email, 
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+              readOnly: true,  // Membuat input tidak dapat diedit
+              enabled: false, // Membuat input tidak dapat diaktifkan
+            ),
+            SizedBox(height: 5),
+
+
+            TextFormField(
+              initialValue: _photoUrl,  
+              decoration: InputDecoration(
+                labelText: 'Photo URL',
+              ),
+              onChanged: (value) {
+
+                // Tambahkan logika jika perlu ketika nilai berubah
+                // Misalnya, Anda dapat memperbarui tampilan gambar di halaman profil
+
+
+              },
+            ),
+
+
+            
+            ElevatedButton(
+              onPressed: () {
+
+                // Aksi saat tombol ditekan (simpan perubahan disini)
+                
+              },
+              child: Text('Simpan'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
